e59dcf5797869a1f6c44537040cb6807446cd805
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-16 21:21:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
+
 ];
 
-const imageLoad = (url) => {
-  return `<img src='${url}' />`;
-};
 
-const all = async () => {
-  try {
-    const gallery = await Promise.all(images.map((val) => fetch(val.url)));
-    gallery.forEach((val) => {
-      output.insertAdjacentHTML("beforeend", imageLoad(val.url));
-    });
+btn.addEventListener('click', ()=>{
+	const h1 = document.createElement('h1');
+	h1.innerText = 'loading please wait...'
+	output.append(h1)
+	fillWithImg();
+})
 
-    return gallery;
-  } catch (err) {
-    return "failed to load images";
-  }
-};
-
-btn.addEventListener("click", () => {
-  all();
-});
+function fillWithImg(){
+	Promise.all(images)
+	.then((arr)=>{
+		output.innerText = ''
+		arr.forEach((img)=>{
+			const imgEl = document.createElement('img');
+			imgEl.src = img.url;
+			output.append(imgEl);
+		})
+	})
+}
