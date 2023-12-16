fa433e43f033c29395026dfb501b99c42e54441f
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-16 21:16:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
-
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const imageLoad = (url) => {
+  return `<img src='${url}' />`;
+};
+
+const all = async () => {
+  try {
+    const gallery = await Promise.all(images.map((val) => fetch(val.url)));
+    gallery.forEach((val) => {
+      output.insertAdjacentHTML("beforeend", imageLoad(val.url));
+    });
+
+    return gallery;
+  } catch (err) {
+    return "failed to load images";
+  }
+};
+
+btn.addEventListener("click", () => {
+  all();
+});
